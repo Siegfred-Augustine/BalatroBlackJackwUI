e2ee91932674292ed507e00ab551392a62fe1a55
Siegfred-Augustine <rhomersiegfred.agustin254591@gmail.com>
2025-11-26 14:42:46 +0800

    initialized web app using MVC

---
+++ BlackJackBalatro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackJackBalatro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
